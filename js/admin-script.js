4099cd1848082e85d7c6b5f24b6754a3d74c2ed7
adityathok <aditya0thok@gmail.com>
2022-07-02 15:57:05 +0700

    add ajax js

--- js/admin-script.js
+++ js/admin-script.js
@@ -1,5 +1,19 @@
 (function( $ ) {
 	jQuery(document).ready(function($) {	
-        
+        $('.wpnewsgen-run').click(function(){
+            $.ajax({
+                type: 'GET',
+                dataType: 'json',
+                url: 'https://newsapi.org/v2/top-headlines',
+                data: { 
+                    'country': $('#country').val(),
+                    'apiKey': $('#apikey').val(),
+                    'pageSize': $('#page').val(),
+                },
+                success: function(result){                    
+                    console.log(result);
+                }
+            });
+        });
     });
 })( jQuery );
